32fcffe5da0afc907edf91b15f4df567740b4b93
Mike Allison <mikeallison79@gmail.com>
2022-07-25 20:23:12 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './styles.css';
+
+import QuoteBox from './components/QuoteBox';
+
+export default function App() {
+  return (
+    <div className="App">
+      <QuoteBox />
+    </div>
+  );
+}
---
+++ src/components/QuoteBox.js
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react';
+
+import './QuoteBox.css';
+
+const QuoteBox = () => {
+  const [quote, setQuote] = useState();
+  const [author, setAuthor] = useState();
+  const [postingUrl, setPostingUrl] = useState();
+
+  useEffect(() => {
+    fetch('https://api.quotable.io/random')
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((data) => {
+        setQuote(data.content);
+        setAuthor(data.author);
+        setPostingUrl(
+          `https://twitter.com/intent/tweet?text="${data.content}" - ${data.author}`
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const newQuoteHandler = () => {
+    fetch('https://api.quotable.io/random')
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((data) => {
+        setQuote(data.content);
+        setAuthor(data.author);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div id="quote-box">
+      <section id="text-author">
+        <div id="text">"{quote}"</div>
+        <div id="author">- {author}</div>
+      </section>
+      <section id="buttons">
+        <button id="new-quote" onClick={newQuoteHandler}>
+          Load New Quote
+        </button>
+        <a href={postingUrl}>Post to Twitter</a>
+      </section>
+    </div>
+  );
+};
+
+export default QuoteBox;
